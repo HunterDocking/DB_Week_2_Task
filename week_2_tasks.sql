3b3ea324c8e4052362cf83e6b5cdb9f2c6cbdfb7
HunterDocking <48078049+HunterDocking@users.noreply.github.com>
2019-08-06 09:59:35 +1000

    Not letting me create table so im trying this

--- week_2_tasks.sql
+++ week_2_tasks.sql
@@ -1,1 +1,47 @@
-PRINT "test"
+DROP TABLE IF EXISTS dbo.Club;
+DROP TABLE IF EXISTS dbo.Season;
+DROP TABLE IF EXISTS dbo.Player;
+DROP TABLE IF EXISTS dbo.TeamEntry;
+DROP TABLE IF EXISTS dbo.PlayerRegistration;
+
+CREATE TABLE Club(
+ClubName        NVARCHAR(100) PRIMARY KEY
+,ContactName    NVARCHAR(100) NULL
+)
+
+CREATE TABLE Season(
+Year            INT
+,SeasonName      NVARCHAR(6)
+,PRIMARY KEY (Year, SeasonName)
+)
+
+CREATE TABLE Player(
+PlayerID        INT PRIMARY KEY
+,Fname          NVARCHAR(100) NOT NULL
+,Lname          NVARCHAR(100) NOT NULL
+,Phone          NVARCHAR(50) NULL
+)
+
+CREATE TABLE TeamEntry(
+ClubName        NVARCHAR(100)
+,Year           INT
+,SeasonName     NVARCHAR(6)
+,AgeGroup       NVARCHAR(3)
+,TeamNumber     INT
+,PRIMARY KEY (ClubName, Year, SeasonName, AgeGroup, TeamNumber)
+,FOREIGN KEY (ClubName) REFERENCES Club
+,FOREIGN KEY (Year, SeasonName) REFERENCES Season
+)
+
+CREATE TABLE PlayerRegistration(
+PlayerID        INT
+,ClubName       NVARCHAR(100)
+,Year           INT
+,SeasonName     NVARCHAR(6)
+,AgeGroup       NVARCHAR(3)
+,TeamNumber     INT
+,DateRegistered DATE
+,PRIMARY KEY (PlayerID, ClubName, Year, SeasonName, AgeGroup, TeamNumber)
+,FOREIGN KEY (PlayerID) REFERENCES Player
+,FOREIGN KEY (ClubName, Year, SeasonName, AgeGroup, TeamNumber) REFERENCES TeamEntry
+)
